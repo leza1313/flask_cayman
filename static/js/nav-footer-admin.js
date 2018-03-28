1fe7205389fa77c17d575ef85de1061e3d8b7107
David <leza1313@gmail.com>
2018-03-28 19:14:05 +0200

    Fixed style of the footer. Now it goes to the bottom when the content is not big enough to fill the screen. Had to remove this styling when the screen is smaller. Added some checking before changing colors to admin-style. NEED to fix the text-align for the 3D editor

--- static/js/nav-footer-admin.js
+++ static/js/nav-footer-admin.js
@@ -1,6 +1,13 @@
 var fondo = document.getElementById('cover');
-fondo.id='cover-admin';
+if ( fondo ){
+    fondo.id='cover-admin';
+}
 var nav = document.getElementById('mynav');
-nav.setAttribute( "class", "navbar navbar-expand-lg navbar-light bg-primary" );
+if ( nav ){
+    nav.setAttribute( "class", "navbar navbar-expand-lg navbar-light bg-primary" );
+
+}
 var foot = document.getElementById('footer1');
-foot.setAttribute("id","footer-admin");
+if ( foot ){
+    foot.setAttribute("id","footer-admin");
+}
