9a9ebae28fb22226808f37ab5e3542a03af1351b
David <leza1313@gmail.com>
2018-04-12 15:39:30 +0200

    Another trial to see if we can get IPN working

--- paypal_ipn.py
+++ paypal_ipn.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 from werkzeug.datastructures import ImmutableOrderedMultiDict
 
 '''This module processes PayPal Instant Payment Notification messages (IPNs).'''
@@ -9,46 +9,21 @@ paypal_ipn = Blueprint('paypal_ipn', __name__)
 
 @paypal_ipn.route("/paypal_ipn", methods=['POST'])
 def paypal_ipn2():
-    # -*- coding: utf-8 -*-
-    """
-        IPN Engine
-        ~~~~~~~~~~
-        PayPal's Instant Payment Notification (IPN) helps integrate PayPal more
-        deeply into your web application.  When activated, PayPal will send a
-        POST request to a specified URL when a transaction's status changes.
-        The most obvious use is to trigger a state change, enable an account,
-        download, or trigger an email when a payment is made.
+    try:
+        arg = ''
+        request.parameter_storage_class = ImmutableOrderedMultiDict
+        values = request.form
+        for x, y in values.iteritems():
+            arg += "&{x}={y}".format(x=x, y=y)
 
-        To validate PayPals POST data, the data must be sent back to PayPal
-        in exactly the same order, prefixed with 'cmd=_notify-validate'. If
-        valid PayPal responds with 'VERIFIED' if not it returns 'INVALID'.
-        Requires: flask, requests
-    """
-    headers = request.headers
-    params = request.form
-    app.logger.warning("{}{}".format(headers,params))
-
-    arg = ''
-    #: We use an ImmutableOrderedMultiDict item because it retains the order.
-    request.parameter_storage_class = ImmutableOrderedMultiDict()
-    values = request.form
-    for x, y in values.iteritems():
-        arg += "&{x}={y}".format(x=x, y=y)
-
-    validate_url = 'https://www.sandbox.paypal.com' \
-                   '/cgi-bin/webscr?cmd=_notify-validate{arg}' \
-        .format(arg=arg)
-
-    print ('Validating IPN using {url}'.format(url=validate_url))
-
-    r = requests.get(validate_url)
-
-    if r.text == 'VERIFIED':
-        print ("PayPal transaction was verified successfully.")
-        # Do something with the verified transaction details.
-        payer_email = request.form.get('payer_email')
-        print ("Pulled {email} from transaction".format(email=payer_email))
-    else:
-        print ('Paypal IPN string {arg} did not validate'.format(arg=arg))
-
-    return r.text
+        validate_url = 'https://www.sandbox.paypal.com' \
+                       '/cgi-bin/webscr?cmd=_notify-validate{arg}' \
+            .format(arg=arg)
+        r = requests.get(validate_url)
+        if r.text == 'VERIFIED':
+            app.logger.warning("VERIFIED")
+        else:
+            app.logger.warning("INVALID")
+        return r.text
+    except Exception as e:
+        return str(e)
