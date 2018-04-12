4ba29803d19a9dd7509581d34908028d54e3bc09
David <leza1313@gmail.com>
2018-04-12 17:57:23 +0200

    Added pedidos table to DB, added model, new paypal button. Attempt to insert orders into pedidos

---
+++ models/pedidos.py
@@ -0,0 +1,62 @@
+from connection import db
+
+class PedidosModel(db.Model):
+    __tablename__ = 'pedidos'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    numero_serie = db.Column(db.String(24))
+    modelo = db.Column(db.String(24))
+    acabado = db.Column(db.String(24))
+    pastillas = db.Column(db.String(24))
+    puente = db.Column(db.String(24))
+    electronica = db.Column(db.String(24))
+    clavijero = db.Column(db.String(24))
+
+    nombre = db.Column(db.String(24))
+    direccion = db.Column(db.String(24))
+    telefono = db.Column(db.String(24))
+    email = db.Column(db.String(24))
+
+    precio = db.Column(db.String(24))
+    fecha = db.Column(db.DateTime)
+    observaciones = db.Column(db.Text)
+
+    def __init__(self,numero_serie,modelo,acabado,pastillas,puente,electronica,clavijero
+                 , nombre,direccion,telefono,email
+                 , precio,fecha,observaciones):
+        self.numero_serie=numero_serie
+        self.modelo=modelo
+        self.acabado=acabado
+        self.pastillas=pastillas
+        self.puente=puente
+        self.electronica=electronica
+        self.clavijero=clavijero
+
+        self.nombre=nombre
+        self.direccion=direccion
+        self.telefono=telefono
+        self.email=email
+
+        self.precio=precio
+        self.fecha=fecha
+        self.observaciones=observaciones
+
+    def json(self):
+        #TODO
+        pass
+
+    @classmethod
+    def find_by_name(cls,name):
+        return cls.query.filter_by(nombre=name).first()
+
+    def insert_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.commit()
--- paypal_ipn.py
+++ paypal_ipn.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, current_app
 from werkzeug.datastructures import ImmutableOrderedMultiDict
+from models.pedidos import PedidosModel
 
 '''This module processes PayPal Instant Payment Notification messages (IPNs).'''
 from flask import current_app as app
@@ -20,8 +21,35 @@ def paypal_ipn2():
                    '/cgi-bin/webscr?cmd=_notify-validate{arg}' \
         .format(arg=arg)
     r = requests.get(validate_url)
+
     if r.text == 'VERIFIED':
         app.logger.warning("VERIFIED")
+        #TODO
+        numero_serie = 112233
+        modelo = "mississpi"
+        acabado="acabado1"
+        pastillas="pastillas1"
+        puente="puente1"
+        electronica="electronica1"
+        clavijero="clavijero1"
+
+        nombre= (values['first_name']+values['last_name'])
+        direccion= (values['address_street']+values['address_city']+values['address_state']+values['address_country'])
+        #TODO
+        telefono=666777888
+        email=values['payer_email']
+
+        precio=values['mc_gross']
+        fecha=values['payment_date']
+
+        #TODO
+        observaciones="Campo para rellenar con observaciones"
+
+        mypedido = PedidosModel(numero_serie,modelo,acabado,pastillas,puente,electronica,clavijero,
+                                nombre,direccion,telefono,email,
+                                precio,fecha,observaciones)
+        mypedido.insert_to_db()
+
     else:
         app.logger.warning("INVALID")
     return r.text
